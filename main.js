736bfee51fb453a63df5031276f7149a67a97a80
Yuhang Jiang <yjiang@student.tgm.ac.at>
2020-02-11 16:05:46 +0100

    removed log of removed variables

--- main.js
+++ main.js
@@ -36,8 +36,6 @@ class SoilMoisture extends utils.Adapter {
 
         // The adapters config (in the instance object everything under the attribute "native") is accessible via
         // this.config:
-        this.log.info('config option1: ' + this.config.option1);
-        this.log.info('config option2: ' + this.config.option2);
 
         /*
         For every state in the system there has to be also an object of type state
